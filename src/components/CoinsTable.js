963299dd1f0dfacbe3c7f0bd720248e557de4c7d
Mohit Yadav <Mohit.Yadav2@mastercard.com>
2024-06-02 13:31:51 +0530

    Shifted coins state

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -22,21 +22,13 @@ import { numberWithCommas } from "./Banner/Carousel";
 import { Pagination } from "@material-ui/lab";
 
 const CoinsTable = () => {
-  const [coins, setCoins] = useState([]);
-  const [loading, setLoading] = useState(false);
+
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
   const history = useHistory();
 
-  const { currency, symbol } = CryptoState();
-
-  const fetchCoins = async () => {
-    setLoading(true);
-    const { data } = await axios.get(CoinList(currency));
-    setCoins(data);
-    setLoading(false);
-  };
-
+  const { currency, symbol, coins, loading, fetchCoins } = CryptoState();
+  
   useEffect(() => {
     fetchCoins();
     // eslint-disable-next-line react-hooks/exhaustive-deps
